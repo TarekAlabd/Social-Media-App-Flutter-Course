b7b4d951fb95a7c82ba03376579df8094629d511
Tarek Alabd <tarekalabd1@gmail.com>
2025-05-13 07:05:25 +0300

    feat: integration with supabase

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -1,3 +1,5 @@
 class AppConstants {
   static const String appName = 'Social Media App';
+  static const String supabaseUrl = 'https://yhfqwsvajrrqtrcflwxz.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloZnF3c3ZhanJycXRyY2Zsd3h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMzI4MTQsImV4cCI6MjA2MjYwODgxNH0.hhNRfMWF4Gcba2M_SpxfEjCo0Agbuge5sA2i9AICPRo';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:social_media_app/core/utils/app_constants.dart';
 import 'package:social_media_app/core/utils/app_router.dart';
 import 'package:social_media_app/core/utils/app_routes.dart';
 import 'package:social_media_app/core/utils/app_theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: AppConstants.supabaseUrl,
+    anonKey: AppConstants.supabaseAnonKey,
+  );
   runApp(const MyApp());
 }
 
