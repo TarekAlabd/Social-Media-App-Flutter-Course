624c97297477b2047f162341982be05ff2edc6e7
Tarek Alabd <tarekalabd1@gmail.com>
2025-05-31 12:46:49 +0300

    feat: fetch posts and create the post item ui

--- lib/core/utils/app_tables_names.dart
+++ lib/core/utils/app_tables_names.dart
@@ -1,4 +1,6 @@
 class AppTablesNames {
   static const String users = 'users';
   static const String stories = 'stories';
+  static const String posts = 'posts';
+  static const String comments = 'comments';
 }
--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_media_app/core/services/core_auth_services.dart';
+import 'package:social_media_app/features/home/models/post_model.dart';
 import 'package:social_media_app/features/home/models/story_model.dart';
 import 'package:social_media_app/features/home/services/home_services.dart';
 
@@ -26,6 +27,24 @@ class HomeCubit extends Cubit<HomeState> {
       emit(StoriesLoaded(stories));
     } catch (e) {
       emit(StoriesError(e.toString()));
+    }
+  }
+
+  Future<void> fetchPosts() async {
+    emit(PostsLoading());
+    try {
+      final rawPosts = await homeServices.fetchPosts();
+      List<PostModel> posts = [];
+      for (var post in rawPosts) {
+        final userData = await coreAuthServices.getUserData(post.authorId);
+        if (userData != null) {
+          post = post.copyWith(authorName: userData.name, authorImageUrl: userData.imageUrl);
+        }
+        posts.add(post);
+      }
+      emit(PostsLoaded(posts));
+    } catch (e) {
+      emit(PostsError(e.toString()));
     }
   }
 }
